28c8c837b402b20101d0675e43bced412396e1f9
Siberia <angroov@gmail.com>
2025-02-12 16:56:03 +0900

    REF change AUTO to IDENTITY

--- src/main/java/com/game/entity/Player.java
+++ src/main/java/com/game/entity/Player.java
@@ -10,7 +10,7 @@ import java.util.Date;
 public class Player {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
     private Long id;
 
